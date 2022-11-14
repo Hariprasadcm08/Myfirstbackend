a0e5ab5de468ba63efa515ae6e88d15fcc1fd9bb
Hariprasadcm08 <hariprasadcm08@gmail.com>
2022-11-14 11:19:36 +0530

    axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -28,7 +28,7 @@ let getDistricts = async function (req, res) {
             url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
         }
         let result = await axios(options);
-        console.log(result)
+       // console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
     }
@@ -79,7 +79,94 @@ let getOtp = async function (req, res) {
 }
 
 
+     //-----------------1---------------//
+let getByDistrictId = async function (req, res) {
+    try {
+        let distId = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${distId} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${distId}&date=${date}`
+        }
+        let result = await axios(options)
+        //console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+     //---------------------2-------------------//
+
+     const sortbyaccending= async function (req, res){
+        try {
+            const {apikey} = req.query;
+            const cities = ["bengaluru","mumbai", "delhi", "kolkata", "chennai", "london", "moscow"];
+            const temp = [];
+            for (i=0;i< cities.length;i++) {
+                const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=${apikey}`);
+                temp.push({city: cities[i], temp: response.data.main.temp});
+    
+            }
+            temp.sort((a, b) => a.temp - b.temp);  
+            res.status(200).send({msg:temp});
+        } 
+        catch (error) {
+            res.status(500).send({msg:error.message});
+        }
+
+    }
+
+
+    let londonWeather=async function(req,res){
+        try{
+            let city=req.query.city
+            let appId=req.query.appid
+            var options={
+                method:"get",
+                url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}`
+            }
+            let result=await axios(options)
+            let temperature = result.data.main.temp
+            res.status(200).send({msg:temperature})
+}
+catch (error) {
+    res.status(500).send({msg:error.message});
+    
+}
+    }
+    
+        //-------------------3--------------------//
+    
+
+const createMemes = async  function(req, res){
+    try {
+        const template_id = req.query;
+        const text0 = req.query;
+        const text1 = req.query;
+        const username = req.query;
+        const password = req.query;
+
+        const response = await axios.post(`https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`);
+        res.status(200).send({msg:response.data});
+    } catch (error) {
+        res.status(500).send({msg:error.message});
+    }
+}
+
+
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+
+
+module.exports.getByDistrictId = getByDistrictId
+module.exports.sortbyaccending = sortbyaccending
+module.exports.londonWeather = londonWeather
+module.exports.createMemes = createMemes
--- src/routes/route.js
+++ src/routes/route.js
@@ -12,11 +12,12 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
-
+router.get("/cowin/getByDistrictId", CowinController.getByDistrictId)
+router.get("/sortbyaccending", CowinController.sortbyaccending)
+router.get("/londonWeather", CowinController.londonWeather)
+router.post("/createMemes", CowinController.createMemes)
 
 module.exports = router;
